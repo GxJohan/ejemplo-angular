e7ef27b4bdac50ba26e4cd734b026f166e99be79
Johan López <johamax@gmail.com>
2024-10-14 16:35:17 -0500

    v1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadChildren: () => import('./app/login/login.module').then(m => m.LoginModule)
+    },
+    {
+        path: 'registro',
+        loadChildren: () => import('./app/registro/registro.module').then(m => m.RegistroModule)
+    },
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: '/login'
+    }
+];
